91b3e7cba1bb0463aa1212f9d06d4738551f3b8b
Olexandr Lisovyi <microfox141120@outlook.com>
2024-08-17 00:09:23 +0300

    firebase fix

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -3,13 +3,14 @@ import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: import.meta.env.FIREBASE_API_KEY,
-  authDomain: "shatter-45ee0.firebaseapp.com",
-  projectId: "shatter-45ee0",
-  storageBucket: "shatter-45ee0.appspot.com",
-  messagingSenderId: "1049275548430",
-  appId: "1:1049275548430:web:f81fafbaf7ef3cb8b68d5f",
-  measurementId: "G-HFDXFMZE2H",
+  authDomain: "shatter-b0750.firebaseapp.com",
+  projectId: "shatter-b0750",
+  storageBucket: "shatter-b0750.appspot.com",
+  messagingSenderId: "205630338997",
+  appId: "1:205630338997:web:29106394c978505ae78bfc",
+  measurementId: "G-MRSMMR1LK8",
 };
 
+// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(app);
